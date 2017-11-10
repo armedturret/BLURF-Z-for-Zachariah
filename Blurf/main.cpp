b66c15a7b76632768540b15766bc124ae3a8c84a
NoiseGenerator <rfnoisegenerator@gmail.com>
2017-11-10 08:09:05 -0500

    Added Stuff

--- Blurf/main.cpp
+++ Blurf/main.cpp
@@ -17,12 +17,20 @@ void aidensCode(CustomAdven& adv) {
 }
 
 void andrewsCode(CustomAdven& adv) {
-adv.setupChoice("go north", "parttwo");
-adv.addPath(__LINE__, "start", "You find yourself in the middle of a forest that you don't recognize, what do you do?");
-	
-adv.setupChoice("push it", "end");
-adv.setupChoice("push the button", "end");
-adv.addPath(__LINE__, "parttwo", "You find a cave with a button that says \"push me\".");
+adv.setupChoice("go south", "centerofvalley");
+adv.setupChoice("go north", "nope");
+adv.addPath(__LINE__, "start", "You step back into the valley, feeling embarrased. Why did you ever think you could leave? To the south is the main road back into the center of the valley. To the north lies the unknown wilderness.");
+
+//adv.setupChoice("talk to him", "talky");
+adv.setupChoice("shoot him", "pewpew");
+adv.addPath(__LINE__, "centerofvalley", "There\'s nothing for it. You head back into the valley. When you enter, Mr. Loomis looks puzzled at your arrival.\n\"What are you doing here? Go!\", he yells. You wonder if he can even survive on his own. Maybe it\'s better that you\'ve come back. You decide to talk to him.");
+
+//adv.setupChoice("talk to him", "talky");
+adv.setupChoice("shoot him", "pewpew");
+adv.addPath(__LINE__, "nope", "You just can't bring youself to do it. You trudge solemnly back into the valley.");
+
+
+adv.addPath(__LINE__, "pewpew", "You grab the gun from you cart and shoot him in the chest. \"You shot me!\", he screams. But they\'re his last words. He dies of his injuries within seconds.");
 }
 
 void robertosCode(CustomAdven& adv) {
@@ -31,7 +39,7 @@ void robertosCode(CustomAdven& adv) {
 
 int main() {
 	//stuff to appear on start up
-	std::string startUpText = "Filer";
+	std::string startUpText = "You prepare to set out on your journey. The world outside is frightening, but you know you can do it. The only question is, which compass direction should you go?";
 
 	CustomAdven adv;
 	adv.begin();
@@ -74,7 +82,7 @@ int main() {
 		else if (b == "GO EAST") {
 			//Starvation
 			system("cls");
-			adv.setupChoice("fin", "end");
+			adv.setupChoice("", "end");
 			adv.addPath(__LINE__, "start", "You go east for a while but there's nothing there. You eventually run out of supplies and die.");
 			break;
 		}
