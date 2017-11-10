3b1c35bdf897d1e7c8df695315480f564bfd1b5c
NegNebulous <ajbewley1@gmail.com>
2017-11-10 14:13:49 -0500

    Added first choice Finished depressing paragraph v1

--- Blurf/aidensclass.cpp
+++ Blurf/aidensclass.cpp
@@ -10,3 +10,23 @@ aidensclass::aidensclass()
 aidensclass::~aidensclass()
 {
 }
+
+void aidensclass::run(CustomAdven & adv)
+{
+	adv.setupChoice("Pickup Food", "foodpickup1");
+	adv.setupChoice("Pickup", "foodpickup1");
+	adv.setupChoice("yes", "foodpickup1");
+	adv.setupChoice("y", "foodpickup1");
+
+	adv.setupChoice("Don't pickup", "nfoodpickup1");
+	adv.setupChoice("Don't", "nfoodpickup1");
+	adv.setupChoice("no", "nfoodpickup1");
+	adv.setupChoice("n", "nfoodpickup1");
+	adv.addPath(__LINE__, "start", R"(	You begin your journy to the north, you wave good-bye to John as you go on.)");
+
+	adv.setupChoice("Continue", "end");
+	adv.addPath(__LINE__, "foodpickup1", "Gained +1 food!");
+
+	adv.setupChoice("Continue", "end");
+	adv.addPath(__LINE__, "nfoodpickup1", "You decide not to pick up the radiated can of food");
+}
--- Blurf/main.cpp
+++ Blurf/main.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <Windows.h>
 #include <map>
-#include "aidensclass.h"
 //TESTY MCTEST
 #include "CustomAdven.h"
+#include <random>
+#include <ctime>
 
 void toCaps(std::string& str)
 {
@@ -14,24 +15,22 @@ void toCaps(std::string& str)
 }
 
 void aidensCode(CustomAdven& adv) {
-	aidensCode code;
-	code.run(adv);
-	adv.setupChoice("Pickup Food", "foodpickup1");
-	adv.setupChoice("Pickup", "foodpickup1");
-	adv.setupChoice("yes", "foodpickup1");
-	adv.setupChoice("y", "foodpickup1");
+	adv.setupChoice("go into the store", "store1");
+	adv.setupChoice("go into store", "store1");
+	adv.setupChoice("go store", "store1");
+	adv.setupChoice("go s", "store1");
+	adv.setupChoice("store", "store1");
+	adv.setupChoice("s", "store1");
 
-	adv.setupChoice("Don't pickup", "nfoodpickup1");
-	adv.setupChoice("Don't", "nfoodpickup1");
-	adv.setupChoice("no", "nfoodpickup1");
-	adv.setupChoice("n", "nfoodpickup1");
-	adv.addPath(__LINE__, "start", R"(	You begin your journy to the north, you wave good-bye to John as you go on.)");
+	adv.setupChoice("go into the library", "library1");
+	adv.setupChoice("go into library", "library1");
+	adv.setupChoice("go library", "library1");
+	adv.setupChoice("go l", "library1");
+	adv.setupChoice("l", "library1");
+	adv.setupChoice("library", "library1");
+	adv.addPath(__LINE__, "start", R"(    You begin heading NORTH waving goodbye to John as you go. After some time you make it all the way around the mounta-in, John has been out of sight for some time now. You have finally dragged the wagon all the way to Odgen town, you're  afraid to look into buildings because you know whats inside. You walk down the streets that are speckled with little    cute birdie corpses. You try not to think about it. You look up and notice some buildings, a store and a Library.)");
 
-	adv.setupChoice("Continue", "end");
-	adv.addPath(__LINE__, "foodpickup1", "Gained +1 food!");
 
-	adv.setupChoice("Continue", "end");
-	adv.addPath(__LINE__, "nfoodpickup1", "You decide not to pick up the radiated can of food");
 }
 
 void andrewsCode(CustomAdven& adv) {
