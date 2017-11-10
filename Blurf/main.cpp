df315a72af32407153622c098f440cc0a4561696
Robert White <rjwhitenh@outlook.com>
2017-11-10 18:09:44 -0500

    Fixed Things

--- Blurf/main.cpp
+++ Blurf/main.cpp
@@ -1,10 +1,35 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
-
-#include "andrews_code.h"
+#include <Windows.h>
+#include <map>
+#include <fstream>
+//TESTY MCTEST
 #include "CustomAdven.h"
+
+#include "andrews_class.h"
+
+std::string getFileContents(std::ifstream& File)
+{
+	std::string Lines = "";        //All lines
+
+	if (File)                      //Check if everything is good
+	{
+		while (File.good())
+		{
+			std::string TempLine;                  //Temp line
+			std::getline(File, TempLine);        //Get temp line
+			TempLine += "\n";                      //Add newline character
+
+			Lines += TempLine;                     //Add newline
+		}
+		return Lines;
+	}
+	else                           //Return error
+	{
+		return "ERROR File does not exist.";
+	}
+}
 
 void toCaps(std::string& str)
 {
@@ -13,21 +38,19 @@ void toCaps(std::string& str)
 }
 
 void aidensCode(CustomAdven& adv) {
-
 }
 
 void andrewsCode(CustomAdven& adv) {
-	andrews_code code;
+	andrews_class code;
 	code.run(adv);
 }
 
 void robertosCode(CustomAdven& adv) {
-
 }
 
 int main() {
 	//stuff to appear on start up
-	std::string startUpText = "Filler";
+	std::string startUpText = "You are playing as Anne Burden from the Robert C. O'Briens\' novel Z for Zacharia. You have just left the valley and are a safe distance away from the insane John Loomis. Now, you must choose which direction to go in. He is yelling that to the west he saw birds. Now you simply need to choose a direction to go in.";
 
 	CustomAdven adv;
 	adv.begin();
@@ -45,33 +68,42 @@ int main() {
 
 	std::string b;
 	while (true) {
+
 		std::cout << startUpText << std::endl;
 		std::getline(std::cin, b);
 
 		toCaps(b);
 		if (b == "GO NORTH") {
-			system("cls");
-			//Go north is where loomis came from (apocalyptic setting)
+			adv.clearScreen();
+			//Go north is where loomis came from (apocolyptic setting)
 			aidensCode(adv);
 			break;
 		}
 		else if (b == "GO SOUTH") {
 			//Back to the valley
-			system("cls");
+			adv.clearScreen();
 			andrewsCode(adv);
 			break;
 		}
 		else if (b == "GO WEST") {
 			//Follow the 'birds'
-			system("cls");
+			adv.clearScreen();
 			robertosCode(adv);
 			break;
 		}
 		else if (b == "GO EAST") {
 			//Starvation
-			system("cls");
-			adv.setupChoice("", "end");
-			adv.addPath(__LINE__, "start", "You go east for a while but there's only a vast, empty wilderness. You eventually run out of supplies and die.");
+			adv.clearScreen();
+			adv.setupChoice("q", "end");
+			adv.addPath(__LINE__, "start", "You go east for a while but there's nothing there. You eventually run out of supplies and die.\nType q to quit.");
+			break;
+		}
+		else if (b == "CRIPPLETRON") {
+			std::ifstream Reader("Surprise.txt");
+
+			adv.setupChoice("q", "end");
+			adv.addPath(__LINE__, "start", getFileContents(Reader)+"\nType q to quit.");
+			
 			break;
 		}
 		
