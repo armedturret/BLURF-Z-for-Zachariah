3356df013d25594bb39522a0ef350b8d48bbd366
armedturret <rjwhitenh@outlook.com>
2017-11-30 12:29:15 -0500

    Got rid of shell

--- UpdateBranches.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git checkout origin/Andrew\'s-Bit -- Blurf/andrews_class.cpp Blurf/andrews_class.h
-git checkout origin/AidensBranch -- Blurf/AidensCode.cpp Blurf/AidensCode.h
-git commit -m "Updated idiv code"
-git push origin master
--- UpdateTextEngine.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git checkout origin/master -- Blurf/CustomAdven.cpp Blurf/CustomAdven.h Blurf/main.cpp
