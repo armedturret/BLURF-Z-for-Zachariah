81db2f0eeb773f3fc46fdc2ff736b2e8d39bdd96
NegNebulous <ajbewley1@gmail.com>
2017-12-02 11:56:47 -0500

    basically finished

--- Blurf/AidensCode.cpp
+++ Blurf/AidensCode.cpp
@@ -66,13 +66,44 @@ void AidensCode::run(CustomAdven & adv)
 	adv.addPath(__LINE__, "library1", R"(	You decide to go to the library and get away from the death for a minute. The walls are lined with shelves of   books, each shelf is labeled "Fiction", "Non-Fiction", "Science-Fiction".)");
 
 	adv.setupChoice("get Drink", "exitstore1");
+	adv.setupChoice("take Drink", "exitstore1");
 
 	adv.setupChoice("Get food", "exitstore1");
+	adv.setupChoice("take food", "exitstore1");
 	adv.addPath(__LINE__, "store1", R"(   You decide to go to the store and get away from the death for a minute. The walls are lined with shelves of food and drinks.)");
 
 	adv.setupChoice("poop", "end");
-	adv.addPath(__LINE__, "exitlibrary1", R"(Poop)");
+	adv.addPath(__LINE__, "exitlibrary1", R"(   After you exit the library you are attacked by some bandits, they yell at you for medicine. Give, fight, run.)");
 
-	adv.setupChoice("poop", "end");
-	adv.addPath(__LINE__, "exitstore1", R"(Poop)");
+	adv.setupChoice("give", "give1");
+	adv.setupChoice("give med", "give1");
+	adv.setupChoice("give medicine", "give1");
+
+	adv.setupChoice("attack", "attack1");
+	adv.setupChoice("attack them", "attack1");
+	adv.setupChoice("attack bandit", "attack1");
+	adv.setupChoice("fight", "attack1");
+	adv.setupChoice("fight them", "attack1");
+	adv.setupChoice("fight bandit", "attack1");
+
+	adv.setupChoice("run", "run1");
+	adv.addPath(__LINE__, "exitstore1", R"(   After you exit the store you are attacked by some bandits, they yell at you for medicine. Give, fight, run.)");
+
+	adv.setupChoice("quit", "end");
+	adv.addPath(__LINE__, "run1", R"(   You decide to run from them they noticed and started to chase you, you hide in a bush and they lose you, but you fall backward into a ravine. It's bad you broke your back for sure, it really hurts and you can’t move much, now you're without food, water, your book or anything else in the wagon. Type quit to exit.)");
+
+	adv.setupChoice("quit", "end");
+	adv.addPath(__LINE__, "attack1", R"(   You decide to attack the bandits, they have an advantage in every way and they kill you and take all your stuff. Type quit to exit.)");
+
+	adv.setupChoice("c", "continue1");
+	adv.addPath(__LINE__, "give1", R"(   You decide to give them the medicine, you tell them how you had used most of it all already, they just sighed. They said that they had been camping out here trying to get radiation medicine for a while, they’re all very sick since they don’t have a suit or anything. You tell them you'll come back for them with medicine as soon as you can. So now in the morning, you head further north for the lab John worked in before. Type c to continue.)");
+
+	adv.setupChoice("open door", "continue2");
+	adv.setupChoice("open", "continue2");
+	adv.setupChoice("use door", "continue2");
+	adv.setupChoice("use lever", "continue2");
+	adv.addPath(__LINE__, "continue1", R"(  It took quite a while but you finally reached the base of the mountain, at the top, there was a single door with a lever flat against the door.)");
+
+	adv.setupChoice("filler", "end");
+	adv.addPath(__LINE__, "continue2", R"(   You pull the lever to one side and the door comes loose, its big enough to bring the wagon in. Inside is a large room and some tables lining the walls in the back there was another door, behind it is the living quarters on one of the beds there was a rotting corpse, it must have been Edward. that was the best place for him. In here there was a shower, you can finally wash and take the suit off after a month. You sleep here, you’ll get what you need in the morning.)");
 }
